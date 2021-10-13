083f4332953a0e0cfd42aaab970740a571ff0f6a
henriquenobre <henriquenobr@gmail.com>
2021-10-13 19:49:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const ModalDelete ={
+    open(){
+        document.querySelector('.modal-delete').classList.add('on')    
+    },
+    close(){
+        document.querySelector('.modal-delete').classList.remove('on')
+    }
+}  
+const Modaladd ={
+    open(){
+        document.querySelector('.modal-add').classList.add('on')    
+    },
+    close(){
+        document.querySelector('.modal-add').classList.remove('on')
+    }
+}  
